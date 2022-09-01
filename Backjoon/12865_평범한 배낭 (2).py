8bbad3be1f4f519ba1d213c0534d3b96873fbda5
na0ngkim <ny3021@gmail.com>
2022-09-02 03:18:12 +0900

    20220901 알고리즘

--- Backjoon/12865_평범한 배낭 (2).py
+++ Backjoon/12865_평범한 배낭 (2).py
@@ -10,9 +10,8 @@ d = [0 for _ in range(k + 1)]
 
 for item in arr:
     w, v = item
-    # 최대 값 k부터 역순으로 들어갈 물건의 무게 w까지 순회
+
     for i in range(k, w - 1, -1):
         d[i] = max(d[i], d[i - w] + v)
-    # 해당 위치의 최대 가치 d[i]와 들어갈 물건의 무게 w만큼 이전의
-    # 최대 가치 d[i - w]에 물건의 가치 v를 더한 값 중에 최대 값
+
 print(d[-1])
