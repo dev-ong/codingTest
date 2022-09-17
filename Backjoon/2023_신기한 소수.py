4210c23a49bcb7bc4c454c30ce4b77ae1e3a5bfa
na0ngkim <ny3021@gmail.com>
2022-09-17 21:33:36 +0900

    20220917 알고리즘

---
+++ Backjoon/2023_신기한 소수.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+
+def find(x):
+    for i in range(2, int(x**0.5) + 1):
+        if int(x) % i == 0:
+            return False
+    return True
+
+
+def dfs(num):
+    # n 자릿수 도달 시 멈춤
+    if len(str(num)) == n:
+        print(num)
+    else:
+        for i in range(10):
+            temp = num * 10 + i
+            # 10 곱하고 i 더해서 자릿수 늘린 수가 소수일때만
+            if find(temp):
+                dfs(temp)
+
+# 한자리 숫자 중에 소수로 시작
+dfs(2)
+dfs(3)
+dfs(5)
+dfs(7)
